b3cd228df964a3807a02b41342985815dd236a0c
kyoodong <wind.dong.dream@gmail.com>
2020-11-21 06:59:36 +0900

    주석

--- alloc.c
+++ alloc.c
@@ -12,6 +12,7 @@ char *map_addr;
 int page_table[PAGE_TABLE_SIZE];
 
 int init_alloc() {
+	// PRIVATE, ANONYMOUS 로 메모리 맵핑
 	map_addr = (char*) mmap((void*) 0, PAGESIZE, PROT_READ | PROT_WRITE,
 			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
@@ -20,6 +21,10 @@ int init_alloc() {
 		return -2;
 	}
 
+	// 페이지 가용 여부를 초기화
+	// -1 이면 사용 가능
+	// -1 이 아닌 정수는 페이지의 범위를 의미.
+	// 예를 들어 page_table[10] = 20 이라면 10 ~ 20까지가 하나의 chunk 로 사용되는 중임을 의미
 	for (int i = 0; i < PAGE_TABLE_SIZE; i++) {
 		page_table[i] = -1;
 	}
@@ -28,9 +33,11 @@ int init_alloc() {
 }
 
 int cleanup() {
+	// 맵핑 해제 전 sync
 	if (msync(map_addr, PAGESIZE, MS_SYNC) < 0)
 		return -1;
 
+	// 맵핑 해제
 	if (munmap(map_addr, PAGESIZE) < 0)
 		return -2;
 	return 0;
@@ -43,11 +50,13 @@ char *alloc(int bufsiz) {
 		return NULL;
 	}
 
+	// 할당받은 4KB 이상의 페이지를 요구한 경우
 	if (bufsiz > PAGESIZE) {
 		fprintf(stderr, "4KB 넘음\n");
 		return NULL;
 	}
 
+	// 논리적 페이지 단위로 변환
 	int count = bufsiz / MINALLOC;
 	int page_offset;
 
@@ -62,17 +71,19 @@ char *alloc(int bufsiz) {
 			count = bufsiz / MINALLOC;
 		}
 
+		// 최초 적합
 		if (count == 0) {
 			break;
 		}
 	}
 
-	// 여유공간이 부족한 경우
+	// 적절한 공간을 찾지못함. 즉 여유공간이 부족한 경우
 	if (count > 0) {
 		fprintf(stderr, "여유공간부족\n");
 		return NULL;
 	}
 
+	// 페이지 테이블 상에 사용 여부(사용)를 기록
 	count = bufsiz / MINALLOC;
 	for (int i = 0; i < count; i++) {
 		page_table[page_offset + i] = page_offset + count;
@@ -85,6 +96,7 @@ void dealloc(char *buffer) {
 	int offset = (buffer - map_addr) / MINALLOC;
 	int size = page_table[offset];
 
+	// 페이지 테이블 상에 사용 여부(미사용)를 기록
 	for (int i = offset; i < size; i++) {
 		page_table[i] = -1;
 	}
--- ealloc.c
+++ ealloc.c
@@ -17,9 +17,16 @@ struct page {
 struct page pages[PAGE_NUM];
 int cur_page_size;
 
+
+/**
+  * @param p 검색할 페이지 (4KB)
+  * @param bufsiz 요청한 메모리의 크기
+  */
 int get_offset(struct page *p, int bufsiz) {
 	int count = bufsiz / MINALLOC;
 	int index = 0;
+
+	// 최초적합
 	for (int i = 0; i < PAGE_TABLE_SIZE; i++) {
 		if (p->page_table[i] == -1) {
 			if (count == bufsiz / MINALLOC) {
@@ -30,6 +37,7 @@ int get_offset(struct page *p, int bufsiz) {
 			count = bufsiz / MINALLOC;
 		}
 
+		// 최초 적합
 		if (count == 0)
 			return index;
 	}
@@ -38,6 +46,7 @@ int get_offset(struct page *p, int bufsiz) {
 }
 
 void init_alloc() {
+	// 페이지 테이블 초기화
 	for (int i = 0; i < PAGE_NUM; i++) {
 		memset(pages[i].page_table, -1, PAGE_TABLE_SIZE);
 		pages[i].mem = NULL;
@@ -45,21 +54,29 @@ void init_alloc() {
 }
 
 char *alloc(int bufsiz) {
+	// 256 의 배수가 아닌 메모리를 요청한 경우
 	if (bufsiz % MINALLOC != 0) {
 		fprintf(stderr, "%d 배수가 아님\n", MINALLOC);
 		return NULL;
 	}
 
-	if (bufsiz > 4096) {
+	// PAGESIZE 보다 큰 메모리를 요청한 경우
+	if (bufsiz > PAGESIZE) {
 		fprintf(stderr, "4096 넘음\n");
 		return NULL;
 	}
 	
 	int page_index = -1;
 	int offset = -1;
+
+	// 현재까지 할당된 모든 페이지 내에서 요청한 메모리를 할당할 수 있는지 검색
 	for (int i = 0 ; i < cur_page_size; i++) {
+		// 요쳥한 페이지 할당 가능한지 검색
 		offset = get_offset(pages + i, bufsiz);
+
+		// 할당 가능한 경우
 		if (offset != -1) {
+			// 페이지 테이블에 사용 여부(사용)을 기록한 뒤 리턴
 			int size = bufsiz / MINALLOC;
 			for (int j = 0; j < size; j++) {
 				pages[i].page_table[offset + j] = offset + size;
@@ -68,17 +85,27 @@ char *alloc(int bufsiz) {
 		}
 	}
 
+	///////////////// 현재까지 할당된 모든 페이지에서 요쳥한 메모리를 할당할 수 없음
+
+
+	// 이미 4개의 페이지를 모두 할당하여 더 이상 할당할 수 없는 상태
 	if (cur_page_size == PAGE_NUM)
 		return NULL;
 
+	///////////////// 아직 추가 페이지를 할당할 수 있는 상태
+
+
+	// 추가 페이지 할당 작업
 	pages[cur_page_size].mem = mmap((void *) 0, PAGESIZE, PROT_READ | PROT_WRITE,
 			MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
+	// 추가 페이지를 할당했을 때는 0번 주소부터 바로 할당처리하여 리턴
 	int size = bufsiz / MINALLOC;
 	for (int j = 0; j < size; j++) {
 		pages[cur_page_size].page_table[j] = size;
 	}
 
+	// 현재 페이지 할당 수 + 1
 	cur_page_size++;
 
 	return pages[cur_page_size - 1].mem;
@@ -87,8 +114,12 @@ char *alloc(int bufsiz) {
 void dealloc(char *buffer) {
 	int page_index = -1;
 	int offset = -1;
+
+	// 몇 번 페이지에서 할당된 메모리인지 검색
 	for (int i = 0; i < cur_page_size; i++) {
 		offset = buffer - pages[i].mem;
+
+		// 메모리 기준 주소와의 차가 4096 미만이면 해당 페이지임을 의미
 		if (offset >= 0 && offset < PAGESIZE) {
 			page_index = i;
 			break;
@@ -97,6 +128,7 @@ void dealloc(char *buffer) {
 
 	offset /= MINALLOC;
 
+	// 페이지 테이블에 사용 여부(미사용)를 기록
 	int target = pages[page_index].page_table[offset];
 	for (int i = offset; i < target; i++) {
 		pages[page_index].page_table[i] = -1;
